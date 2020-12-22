40a1ccecd592f9737b4c5b7824b1a1432f40d030
Jakob de Guzman <55227471+Proximitynow19@users.noreply.github.com>
2020-12-23 09:55:21 +1300

    different encoder

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 let chips = [];
 
-if (new URLSearchParams(window.location.search).get("chips")) {
+if (new URLSearchParams(window.location.search).get("c")) {
   try {
     chips = JSON.parse(
-      atob(new URLSearchParams(window.location.search).get("chips"))
+      de(new URLSearchParams(window.location.search).get("c"))
     );
+
+    if (typeof chips !== "object") {
+      chips = [];
+
+      window.history.pushState(null, null, `/logic.html`);
+    }
   } catch {
     window.history.pushState(null, null, `/logic.html`);
   }
@@ -22,6 +28,42 @@ let nodeID = 1;
 let board = [];
 let startDraw = undefined;
 let holdingChip = undefined;
+
+function en(c) {
+  var x = "charCodeAt",
+    b,
+    e = {},
+    f = c.split(""),
+    d = [],
+    a = f[0],
+    g = 256;
+  for (b = 1; b < f.length; b++)
+    (c = f[b]),
+      null != e[a + c]
+        ? (a += c)
+        : (d.push(1 < a.length ? e[a] : a[x](0)), (e[a + c] = g), g++, (a = c));
+  d.push(1 < a.length ? e[a] : a[x](0));
+  for (b = 0; b < d.length; b++) d[b] = String.fromCharCode(d[b]);
+  return d.join("");
+}
+
+function de(b) {
+  var a,
+    e = {},
+    d = b.split(""),
+    c = (f = d[0]),
+    g = [c],
+    h = (o = 256);
+  for (b = 1; b < d.length; b++)
+    (a = d[b].charCodeAt(0)),
+      (a = h > a ? d[b] : e[a] ? e[a] : f + c),
+      g.push(a),
+      (c = a.charAt(0)),
+      (e[o] = f + c),
+      o++,
+      (f = a);
+  return g.join("");
+}
 
 function newBoard() {
   nodes = [];
@@ -782,7 +824,7 @@ function newChip(name) {
   window.history.pushState(
     null,
     null,
-    `?chips=${encodeURIComponent(btoa(JSON.stringify(chips)))}`
+    `?c=${encodeURIComponent(en(JSON.stringify(chips)))}`
   );
   newBoard();
 }
